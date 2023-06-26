244c5a4df275f984e2957c70e54a71db22067aa0
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-06-26 09:39:51 +0400

    delete extra code

--- jscode.js
+++ jscode.js
@@ -2,9 +2,3 @@
 function hello() {
     document.getElementById("output").textContent = "Hello World";
 }
-=======
-function hello() {
-    return "Hello, World!";
-}
-document.getElementById("output").textContent = hello();
->>>>>>> 801927fe96935f4118f2876ab4ea78c1781c24c1
